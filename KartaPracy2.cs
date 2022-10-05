7283758dcbc07779c300bd7be8b1e6514ede264b
dejvid987 <114577003+dejvid987@users.noreply.github.com>
2022-10-05 11:06:00 +0200

    zadanai 1-7

---
+++ KartaPracy2.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Zad 1
+
+            /*
+            int a = int.Parse(Console.ReadLine());
+            if (a % 3 == 0)
+            {
+                Console.WriteLine("TAK");
+            }
+            else
+            {
+                Console.WriteLine("NIE");
+            }
+            */
+
+            // Zad 2
+
+            /*int a;
+            a = int.Parse(Console.ReadLine());
+            if (a > 99 && a < 1000 && a % 17 == 0)
+            {
+                Console.WriteLine("Jest");
+            }
+            else
+            {
+                Console.WriteLine("Nie jest");
+            }*/
+
+            // Zad 3
+
+            /*
+            int a = int.Parse(Console.ReadLine());
+            if (a > 17)
+            {
+                Console.WriteLine("Osoba jest pełnoletnia");
+            }
+            else
+            {
+                Console.WriteLine("Osoba nie jest pełnoletnia");
+            }
+            */
+
+            // Zad 4
+
+
+            /*
+            const int limit = 20;
+            int waga;
+            waga = int.Parse(Console.ReadLine());
+            if (waga >= limit)
+            {
+                Console.WriteLine("Ciężarówka nie przejedzie przez most");
+            }
+            else
+            {
+                Console.WriteLine("Ciężarówka przejedzie przez most");
+            }*/
+
+            // Zad 5
+
+            /*int a, b, c;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            if (c > b && c < a)
+            {
+                Console.WriteLine("Liczba trzecia mieści się między dwoma innymi liczbami");
+            }
+            else if (c < b && c > a)
+            {
+                Console.WriteLine("Liczba trzecia mieści się między dwoma innymi liczbami");
+            }
+            else
+            {
+                Console.WriteLine("Liczba trzecia nie mieści się między dwoma liczbami");
+            }*/
+
+            // Zad 6
+
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+            if ((Math.Pow(a, p) - a) % p == 0)
+            {
+                Console.WriteLine("TAK");
+            }
+            else
+            {
+                Console.WriteLine("NIE");
+            }*/
+
+            // Zad 7
+            /*
+            int p = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int s = int.Parse(Console.ReadLine());
+            if ((p + 3 * s) >=k)
+            {
+                Console.WriteLine("TAK");
+            }
+            else
+            {
+                Console.WriteLine("NIE");
+            }
+            */
+
+        }
+    }
+}
